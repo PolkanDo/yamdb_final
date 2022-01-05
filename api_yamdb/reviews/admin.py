21350d3d2d135059c4cf1cc13e971aa5fb148742
PolkanDo <mrmarmon@mail.ru>
2022-01-06 00:28:07 +0300

    add test with flake8

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from reviews.models import Category, Genre, Title, User
-from reviews.models import Review, Comment
+from reviews.models import Category, Comment, Genre, Review, Title, User
 
 
 class UserAdmin(admin.ModelAdmin):
